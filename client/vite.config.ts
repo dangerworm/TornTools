e4723f0eb47e83ef2bc3836ffcc0cc51da3c025c
Drew <dangerworm@gmail.com>
2025-11-19 21:56:41 +0000

    Correction to Vite variable name

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, new URL("./", import.meta.url).pathname, "");
-  const basePath = env.VITE_BASE_PATH ?? "/";
+  const basePath = (env.VITE_API_BASE_URL ?? "/") + "/api" ;
 
   return {
     base: basePath,
