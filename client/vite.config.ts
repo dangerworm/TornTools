eb6dac7e112ce6b7f9d247fbcd7b0f3720f8f237
Drew <dangerworm@gmail.com>
2025-11-19 22:13:52 +0000

    Remove problematic base path setting from vite config.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, new URL("./", import.meta.url).pathname, "");
-  const basePath = env.VITE_API_BASE_URL ?? "/";
-
+export default defineConfig(() => {
   return {
-    base: basePath,
+    base: "/",
     plugins: [react()],
   };
 });
