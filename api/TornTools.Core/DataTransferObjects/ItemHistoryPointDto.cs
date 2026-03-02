579278269bc1a1c7f41484ccbb9deef3b88ad9e8
Drew Morgan <dangerworm@gmail.com>
2026-03-02 14:17:23 +0000

    Refactor date handling to use DateTimeOffset for timestamps and update SQL query parameters for improved accuracy.

--- api/TornTools.Core/DataTransferObjects/ItemHistoryPointDto.cs
+++ api/TornTools.Core/DataTransferObjects/ItemHistoryPointDto.cs
@@ -2,7 +2,7 @@ namespace TornTools.Core.DataTransferObjects;
 
 public class ItemHistoryPointDto
 {
-    public required DateTime Timestamp { get; set; }
+    public required DateTimeOffset Timestamp { get; set; }
     public long? Price { get; set; }
     public int? Velocity { get; set; }
 }
